2677f81ecdd66a4d6adc3263d6cedc1e1a54fb18
patthadon40128@gmail.com <patthadon40128@gmail.com>
2021-08-04 23:28:55 +0700

    first commit

---
+++ Anarchist_Bot.py
@@ -0,0 +1,19 @@
+import discord
+import os
+from discord.ext import commands,tasks
+import asyncio
+import random
+import math
+import datetime
+import time
+
+client = commands.Bot(command_prefix = "?")
+
+@client.event
+async def on_ready():
+    global ConsoleChannel
+    ConsoleChannel = client.get_channel(856882963107282967)
+    await ConsoleChannel.send("{0.user} ready to serve mother Anarchy.".format(client))
+
+TOKEN = "HyLkBKa6LeFzW9Bsau7xI6LepxCKnaGg"
+client.run(TOKEN)
