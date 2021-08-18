9d925f85041b05f020cafd34c4c5d7728dc9ed20
HoAi <art_alys@hotmail.com>
2021-08-18 18:20:29 +0700

    ready to goes public!

--- run_Anrachist_Bot.py
+++ run_Anrachist_Bot.py
@@ -1,4 +1,6 @@
 from Anarchist_Bot import main as run
-use_token = "ODcyNTE1MDQ3ODQxMjI2NzUy.YQq-9Q.G8ILwUBHW9g7e5ZyC4UwaMzfVRI"
+from dotenv import load_dotenv
+import os
+load_dotenv()
 if __name__ == '__main__':
-    run(use_token)
+    run(os.getenv("use_token"))
